7431299eab39136bcfe73582f9d10d93f29a3829
thanhduy1512 <duongduyy1512@gmail.com>
2023-06-27 22:38:49 +0700

    add axios instance and initial first 20 products via API

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -5,43 +5,22 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import axiosInstance from '../api/axios';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
+interface Product {
+  id: number;
+  brand: string;
+  category: string;
+  description: string;
+  price: number;
+  images: string[];
 }
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 11111, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 111111111, 9.0, 37, 4.3),
-  createData('Eclair', 222222, 16.0, 24, 6.0),
-  createData('Cupcake', 333333, 3.7, 67, 4.3),
-  createData('Gingerbread', 44444, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 111111, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 1111112111, 9.0, 37, 4.3),
-  createData('Eclair', 2322222, 16.0, 24, 6.0),
-  createData('Cupcake', 3334333, 3.7, 67, 4.3),
-  createData('Gingerbread', 445444, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 11114411, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 111221112111, 9.0, 37, 4.3),
-  createData('Eclair', 232233222, 16.0, 24, 6.0),
-  createData('Cupcake', 333411333, 3.7, 67, 4.3),
-  createData('Gingerbread', 42245444, 16.0, 49, 3.9),
-];
 
 const DataTable = () => {
   const tableRef = useRef(null);
+
+  const [products, setProducts] = useState<Product[]>([]);
   const onTableScroll = (event: any) => {
     if (!tableRef.current) return;
     const { scrollTop, scrollHeight, clientHeight } = tableRef?.current;
@@ -51,11 +30,25 @@ const DataTable = () => {
     }
   };
 
+  const getProducts = async () => {
+    const data = await axiosInstance.get('', {
+      params: {
+        limit: 20,
+        skip: 0,
+      },
+    });
+    setProducts(data.data.products);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
       <h1>DataTable</h1>
       <TableContainer
-        style={{ overflow: 'scroll', height: 600 }}
+        style={{ overflowY: 'scroll', height: 600 }}
         component={Paper}
         onScroll={onTableScroll}
         ref={tableRef}
@@ -63,26 +56,34 @@ const DataTable = () => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Brand</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Image</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {products.map((row) => (
               <TableRow
-                key={row.name + row.calories}
+                key={row.id + row.brand}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.id}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.brand}</TableCell>
+                <TableCell align="right">{row.category}</TableCell>
+                <TableCell align="right">{row.description}</TableCell>
+                <TableCell align="right">{row.price}</TableCell>
+                <TableCell align="right">
+                  <img
+                    style={{ width: 100, height: 100 }}
+                    src={row.images[0]}
+                    alt={row.images[0]}
+                  />
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
